108f0e7e2fa202493216e99fba0d8f524e8ef186
Wojtec <poninski.w@gmail.com>
2020-07-24 15:53:59 +0200

    slider working

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,8 @@ export default function Home() {
       <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
       <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     </Head> 
+
+    {/* NAVBAR */}
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
       <div className="container">
         <a href="#" className="navbar-brand">Start bootstrap</a>
@@ -44,7 +46,56 @@ export default function Home() {
           </ul>
         </div>
       </div>
-    </nav>   
+    </nav>
+
+    {/* Content */}
+    <section className="container">
+      <div className="row">
+      <div className="col-lg-3">
+        <h1 className="my-4">Shop Name</h1>
+        <div className="list-group">
+          <a href="#" className="list-group-item">Category 1</a>
+          <a href="#" className="list-group-item">Category 2</a>
+          <a href="#" className="list-group-item">Category 3</a>
+        </div>
+      </div>
+      <div className="col-lg-9">
+        <div id="carouselExampleIndicators" className="carousel slide my-4" data-ride="carousel">
+          <ol className="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+          </ol>
+          <div className="carousel-inner" role="listbox">
+            <div className="carousel-item active">
+            <img className="d-block img-fluid" src="http://placehold.it/900x350" alt="First slide"/>
+            </div>
+            <div className="carousel-item">
+              <img className="d-block img-fluid" src="http://placehold.it/900x350" alt="Second slide"/>
+            </div>
+            <div className="carousel-item">
+              <img className="d-block img-fluid" src="http://placehold.it/900x350" alt="Third slide"/>
+            </div>
+          </div>
+          <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span className="sr-only">Previous</span>
+          </a>
+          <a  className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            <span className="sr-only">Next</span>
+          </a>
+        </div>
+
+        <div className="row">
+
+          <div className="col-lg-4 col-md-6 mb-4">
+            
+          </div>
+        </div>
+      </div>
+     </div>
+    </section> 
     </div>
   )
 }
